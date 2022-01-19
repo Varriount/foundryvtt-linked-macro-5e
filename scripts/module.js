27f73c5c15c09d8a529d61f34339d411d9512e2b
DarkDinDoN <maxime.chicault@gmail.com>
2022-01-19 22:28:16 +0100

    Linter

--- scripts/module.js
+++ scripts/module.js
@@ -33,7 +33,7 @@ class LinkedMacro5e {
         const itemToRoll = actorToRoll?.items.get(itemId)
 
         if (!itemToRoll) {
-	        return ui.notifications.warn(game.i18n.format("DND5E.ActionWarningNoItem", { item: itemId, name: actorToRoll?.name ?? "[Not Found]" }))
+          return ui.notifications.warn(game.i18n.format("DND5E.ActionWarningNoItem", { item: itemId, name: actorToRoll?.name ?? "[Not Found]" }))
         }
 
         if (game.modules.get('itemacro')?.active && itemToRoll.hasMacro()) {
